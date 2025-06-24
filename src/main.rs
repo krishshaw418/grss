88b1dc7e1bd0bfff56d4611153a2e3caa6b8dd71
Krish Shaw <shawkrish418@gmail.com>
2025-06-25 01:35:55 +0530

    perf: replace read_to_string with BufReader to reduce memory usage and improve performance

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use std::io::prelude::*;
 use clap::Parser;
+use std::{fs::File, io::BufReader};
 
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(Parser)]
@@ -10,11 +12,23 @@ struct Cli {
 }
 fn main(){
     let args = Cli::parse();
-    let content = std::fs::read_to_string(&args.path).expect("could not read file");
-    
-    for line in content.lines() {
-        if line.contains(&args.pattern) {
-            println!("{}", line);
+    let _result = read_through_buffer(&args.pattern, &args.path);
+}
+
+
+// Implementing BufReader to improve performcance and use less memory while reading large files
+fn read_through_buffer(pattern: &str, path: &std::path::PathBuf) -> std::io::Result<()>{
+    let f = File::open(path)?;
+    let reader = BufReader::new(f);
+    for line_result in reader.lines() {
+        match line_result {
+            Ok(line) => {
+                if line.contains(pattern) {
+                    println!("{}", line);
+                }
+            },
+            Err(_) => println!("Error reading lines!"),
         }
     }
+    Ok(())
 }
