7a9f733cc3fa60de523167a493e7f10ef94e4aa4
Krish Shaw <shawkrish418@gmail.com>
2025-06-25 12:55:00 +0530

    refractor(file-read): using read_to_string to test something

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use std::io::prelude::*;
+// use std::io::prelude::*;
 use clap::Parser;
-use std::{fs::File, io::BufReader};
+use anyhow:: {Context, Result};
+// use std::{fs::File, io::BufReader};
+
 
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(Parser)]
@@ -10,25 +12,32 @@ struct Cli {
     // The path to the file to read
     path: std::path::PathBuf
 }
-fn main(){
+fn main() -> Result<()>{
     let args = Cli::parse();
-    let _result = read_through_buffer(&args.pattern, &args.path);
+    let content = std::fs::read_to_string(&args.path).with_context(|| format!("Could not read file {}", args.path.display()))?;
+
+    for line in content.lines() {
+        if line.contains(&args.pattern){
+            println!("{}", line);
+        }
+    };
+    Ok(())
 }
 
 
 // Implementing BufReader to improve performcance and use less memory while reading large files
-fn read_through_buffer(pattern: &str, path: &std::path::PathBuf) -> std::io::Result<()>{
-    let f = File::open(path)?;
-    let reader = BufReader::new(f);
-    for line_result in reader.lines() {
-        match line_result {
-            Ok(line) => {
-                if line.contains(pattern) {
-                    println!("{}", line);
-                }
-            },
-            Err(_) => println!("Error reading lines!"),
-        }
-    }
-    Ok(())
-}
+// fn read_through_buffer(pattern: &str, path: &std::path::PathBuf) -> std::io::Result<()>{
+//     let f = File::open(path)?;
+//     let reader = BufReader::new(f);
+//     for line_result in reader.lines() {
+//         match line_result {
+//             Ok(line) => {
+//                 if line.contains(pattern) {
+//                     println!("{}", line);
+//                 }
+//             },
+//             Err(_) => println!("Error reading lines!"),
+//         }
+//     }
+//     Ok(())
+// }
